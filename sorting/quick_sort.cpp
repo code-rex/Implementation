29e71f4dc2947ddf50b1febc86e6b639ce43d06a
Sahil kumar <sk19631999@gmail.com>
2019-02-22 03:25:27 +0530

    Update quick_sort.cpp
    Paradigm : Divide and conquer

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -1,12 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+/* This function takes last element as pivot, places 
+    the pivot element at its correct position in sorted 
+    array, and places all smaller (smaller than pivot) 
+    to left of pivot and all greater elements to right 
+    of pivot */
 int partition(int* a,int l,int h)
 {
-	int x=a[h];
-	int i=l-1;
+	int x=a[h]; // pivot 
+	int i=l-1;  // Index of smaller element
 	for(int j=l;j<h;j++)
-		if(a[j]<=x)
-			i++,swap(a[i],a[j]);
+		if(a[j]<=x)                 // If current element is smaller than or equal to pivot
+			{
+			i++;                // increment index of smaller element 
+			swap(a[i],a[j]);
+			}
 	swap(a[i+1],a[h]);
 	return i+1;
 }
