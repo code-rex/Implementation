810fd08f3eae882961c47f273ef04bff2b9dc8b8
Sahil kumar <sk19631999@gmail.com>
2019-04-03 23:55:35 +0530

    Update WEIGHTED_SCHEDULING.cpp

--- CS204/Dynamic Programming/WEIGHTED_SCHEDULING.cpp
+++ CS204/Dynamic Programming/WEIGHTED_SCHEDULING.cpp
@@ -8,6 +8,7 @@ class interval
 	public:
 		int start,finish,weight;
 };
+//Sorting by finish time in ascending order
 bool cmp(interval a,interval b)
 {
 	return a.finish<b.finish;
@@ -36,14 +37,17 @@ int weighted_scheduling(interval a[],int n)
 {
 	if(n==1)
 		return a[n-1].weight;
+	//table[i] stores the profit for jobs till arr[i] (including arr[i]) 
+	//including the first job
 	table[0]=a[0].weight;
 	for(int i=1;i<n;i++)
 	{
-		int include=a[i].weight;
+	    int include=a[i].weight;
+	    //finding the immediate previous job whose finish time is less than start time of current job
 	    binary[i]=binary_search(a,i);
 		if(binary[i]!=-1)
 			include+=table[binary[i]];
-
+	//including or excluding the current job
 		table[i]=max(table[i-1],include);
 	}
 	return table[n-1];
@@ -52,6 +56,7 @@ void find_solution(interval a[],int j)
 {
 	if(j==0)
 		cout<<a[j].start<<" "<<a[j].finish<<" "<<a[j].weight<<endl;
+	
 	else if(a[j].weight+table[binary[j]]>table[j-1])
 		{
 			cout<<a[j].start<<" "<<a[j].finish<<" "<<a[j].weight<<endl;
