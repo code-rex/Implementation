a1cfecc7f707aa7620b0faa7b724df185b974ecb
Sahil kumar <sk19631999@gmail.com>
2019-06-09 00:08:55 +0530

    Update counting sort.cpp

--- sorting/O(N)/counting sort.cpp
+++ sorting/O(N)/counting sort.cpp
@@ -2,6 +2,8 @@
 //time complexity : O(n+k) k=range
 //Counting sort uses a partial hashing to count the occurrence of the data object in O(1).
 
+//The Counting sort is a stable sort i.e., multiple keys with the same value are placed in 
+//the sorted array in the same order that they appear in the input array.
 
 void countSort(vector <int>& arr) 
 { 
@@ -16,7 +18,7 @@ void countSort(vector <int>& arr)
     for(int i = 1; i < count.size(); i++) 
            count[i] += count[i-1]; 
     
-    for(int i = arr.size()-1; i >= 0; i--) 
+    for(int i = arr.size()-1; i >= 0; i--) //1
     {  
          output[ count[arr[i]-min] -1 ] = arr[i];  
               count[arr[i]-min]--;  
@@ -25,3 +27,5 @@ void countSort(vector <int>& arr)
     for(int i=0; i < arr.size(); i++) 
             arr[i] = output[i]; 
 } 
+
+//if at pos 1 i<-1 to n then then the stability no longer holds.
