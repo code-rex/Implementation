0865fef823cd2928220c331b40ef752572192e00
Sahil kumar <sk19631999@gmail.com>
2019-06-19 14:16:42 +0530

    Update Kthsmallest nlogn.cpp

--- Basic/InterviewBit/Checkpoint/Kthsmallest nlogn.cpp
+++ Basic/InterviewBit/Checkpoint/Kthsmallest nlogn.cpp
@@ -1,1 +1,47 @@
+/*
+Find the kth smallest element in an unsorted array of non-negative integers.
+Definition of kth smallest element
+ kth smallest element is the minimum possible n such that there are at least k elements in the array <= n.
+In other words, if the array A was sorted, then A[k - 1] ( k is 1 based, while the arrays are 0 based ) 
+NOTE
+You are not allowed to modify the array ( The array is read only ). 
+Try to do it using constant extra space.
+Example:
+A : [2 1 4 3 2]
+k : 3
+answer : 2
+https://www.interviewbit.com/problems/kth-smallest-element-in-the-array/
+*/
+// Binary Search 
 
+int Solution::kthsmallest(const vector<int> &A, int k) {
+    if (k > A.size())
+        return -1;
+    
+    int lo = *min_element(A.begin(), A.end());
+    int hi = *max_element(A.begin(), A.end());
+    
+    while (lo <= hi)
+    {
+        int mid = lo + (hi - lo)/2;
+        int countLess = 0, countEqual = 0;
+        
+        for (auto i = 0; i<A.size(); ++i)
+        {
+            if (A[i]<mid)
+                ++countLess;
+            else if (A[i] == mid)
+                ++countEqual;
+            if (countLess >= k)
+                break;
+        }
+        
+        if (countLess < k && countLess + countEqual >= k)
+            return mid;
+        else if (countLess >= k)
+            hi = mid - 1;
+        else
+            lo = mid + 1;
+    }
+    return -1;
+}
