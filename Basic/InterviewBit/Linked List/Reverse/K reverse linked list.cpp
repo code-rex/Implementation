428db064788e0f71f881d965100d548155b9504a
Sahil kumar <sk19631999@gmail.com>
2019-07-02 16:36:08 +0530

    Create K reverse linked list.cpp

---
+++ Basic/InterviewBit/Linked List/Reverse/K reverse linked list.cpp
@@ -0,0 +1,42 @@
+/*
+Given a singly linked list and an integer K, reverses the nodes of the
+
+list K at a time and returns modified linked list.
+
+ NOTE : The length of the list is divisible by K 
+Example :
+
+Given linked list 1 -> 2 -> 3 -> 4 -> 5 -> 6 and K=2,
+
+You should return 2 -> 1 -> 4 -> 3 -> 6 -> 5
+
+Try to solve the problem using constant extra space.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ListNode* Solution::reverseList(ListNode* A, int B) {
+    if(B==1)
+        return A;
+    
+    ListNode *prev=NULL,*curr=A,*next=NULL;
+    ListNode *prevfirst=curr;
+    
+    for(int i=0;i<B&&curr;i++)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    if(prevfirst)
+        prevfirst->next=reverseList(curr,B);
+    return prev;
+}
