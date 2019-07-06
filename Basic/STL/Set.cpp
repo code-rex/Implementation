35467b08ffaf1807e4568af3424427b11d99ca24
Sahil kumar <sk19631999@gmail.com>
2019-07-06 14:59:50 +0530

    Create Set.cpp
    For Big-Oh time complexity : http://bigocheatsheet.com/
    References :https://www.geeksforgeeks.org/difference-set-multiset-unordered_set-unordered_multiset/

---
+++ Basic/STL/Set.cpp
@@ -0,0 +1,56 @@
+1. Ordered set (Set is implemented as balanced tree structure)
+	Consider we need a container with the following features:
+	a. add an element, but do not allow duples [duplicates?]
+	b. remove elements
+	c. get count of elements (distinct elements)
+	d. check whether elements are present in set
+	
+	Function: set<int>s 
+	1) s.insert(42)
+	2) s.erase(42)
+	3) int n=s.size()
+	4) Iteration
+		Since set is not a linear container, it’s impossible to take the element in set by index. 
+		Therefore, the only way to traverse the elements of set is to use iterators.
+
+		// Calculate the sum of elements in set
+		set< int > S;
+		// ...
+		int r = 0;
+		for(set< int >::const_iterator it = S.begin(); it != S.end(); it++) {
+		r += *it;
+		}
+	5) set< int > s;
+		// ...
+		if(s.find(42) != s.end()) {
+		// 42 presents in set
+		}
+		else {
+		// 42 not presents in set
+		}
+	6) set< int >::iterator it1, it2;
+	
+		it1 = s.find(10);
+		it2 = s.find(100);
+		// Will work if it1 and it2 are valid iterators, i.e. values 10 and 100 present in set.
+		s.erase(it1, it2); // Note that 10 will be deleted, but 100 will remain in the container
+
+		Set has an interval constructor:
+
+		int data[5] = { 5, 1, 4, 2, 3 };
+		set< int > S(data, data+5);
+		It gives us a simple way to get rid of duplicates in vector, and sort it:
+
+		vector< int > v;
+		// …
+		set< int > s(all(v));
+		vector< int > v2(all(s));
+
+		Here ‘v2′ will contain the same elements as ‘v’ but sorted in ascending order and with duplicates removed.
+		
+2. Unordered_set(unordered_set is implemented using hash table where keys are hashed into indices of this hash table so 
+				it is not possible to maintain an order. All operation on unordered_set takes constant time O(1) on an 
+				average which can go up to linear time in worst case which depends on the internally used hash function 
+				but practically they perform very well and generally provide constant time lookup operation.)
+				
+3. Multi_set(It allows storage of multiple elements)
