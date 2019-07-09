7ea2e98061d2f34d7d46556bbb8369d7aec2a95a
Sahil kumar <sk19631999@gmail.com>
2019-07-09 21:39:27 +0530

    Create Inorder.cpp

---
+++ Basic/InterviewBit/TREE/Inorder.cpp
@@ -0,0 +1,48 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes’ values.
+
+Example :
+Given binary tree
+
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Using recursion is not allowed.
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+*/
+ 
+vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> vector;
+        stack<TreeNode *> stack;
+        TreeNode *pCurrent = root;
+
+        while(!stack.empty() || pCurrent)
+        {
+            if(pCurrent)
+            {
+                stack.push(pCurrent);
+                pCurrent = pCurrent->left;
+            }
+            else
+            {
+                TreeNode *pNode = stack.top();
+                vector.push_back(pNode->val);
+                stack.pop();
+                pCurrent = pNode->right;
+            }
+        }
+        return vector;
+    }
