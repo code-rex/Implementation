464b16fdb26dfc4c9b91f921cebdc996b539815a
Sahil kumar <sk19631999@gmail.com>
2019-07-12 21:40:05 +0530

    Update Merge K sorted list(Heap).cpp

--- Basic/InterviewBit/Heap and Maps/Merge K sorted list(Heap).cpp
+++ Basic/InterviewBit/Heap and Maps/Merge K sorted list(Heap).cpp
@@ -40,3 +40,34 @@ ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
     }
     return head;
 }
+
+
+//OR
+struct CompareNode 
+{
+    bool operator()(ListNode* const & p1, ListNode* const & p2) {
+    // return "true" if "p1" is ordered before "p2", for example:
+        return p1->val > p2->val;
+    }
+};
+ListNode* Solution::mergeKLists(vector<ListNode*> &lists) {
+            ListNode* dummy = new ListNode(0);
+            ListNode* tail = dummy;
+
+            priority_queue<ListNode*,vector<ListNode*>,CompareNode> queue;
+
+            for (int i = 0; i < lists.size(); i++) 
+                    queue.push(lists[i]);
+
+            while (!queue.empty()) {
+                    tail->next = queue.top();
+                    queue.pop();
+                    tail = tail->next;
+
+                    if (tail->next)
+                            queue.push(tail->next);
+            }
+
+            return dummy->next;
+    
+}
