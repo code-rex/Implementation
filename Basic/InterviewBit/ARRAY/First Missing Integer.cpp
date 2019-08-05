5b324a8fa0f930292f87748f431f68bd34e5d393
Sahil kumar <sk19631999@gmail.com>
2019-08-06 01:40:09 +0530

    Update First Missing Integer.cpp

--- Basic/InterviewBit/ARRAY/First Missing Integer.cpp
+++ Basic/InterviewBit/ARRAY/First Missing Integer.cpp
@@ -19,13 +19,14 @@ int Solution::firstMissingPositive(vector<int> &A) {
             
     for(int i=0;i<A.size();i++)
     {
+        //Bound and Positive check
         if(A[i]>A.size() || A[i]<=0)
             continue;
             
         while(A[i]!=A[A[i]-1])
         {
             swap(A[i],A[A[i]-1]);
-            
+            //Bound and Positive check after swapping
             if(A[i]>A.size() || A[i]<=0)
                  break;
         }
