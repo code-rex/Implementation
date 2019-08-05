f3a0ef4eec713d0fdf6482bd04f5d8a5a220a517
Sahil kumar <sk19631999@gmail.com>
2019-08-05 20:08:57 +0530

    Update Level Order.cpp

--- Basic/InterviewBit/GRAPH/Level Order.cpp
+++ Basic/InterviewBit/GRAPH/Level Order.cpp
@@ -57,3 +57,24 @@ vector<vector<int> > Solution::levelOrder(TreeNode* A) {
         res.push_back(x.second);
     return res;
 }
+
+
+//------------2---------------------------
+
+void buildVector(TreeNode *root, int depth, vector<vector<int> > &ret) {
+        if(root == NULL) 
+             return;
+        if(ret.size() == depth)
+            ret.push_back(vector<int>());
+
+        ret[depth].push_back(root->val);
+        buildVector(root->left, depth + 1, ret);
+        buildVector(root->right, depth + 1, ret);
+    }
+
+    vector<vector<int> > Solution::levelOrder(TreeNode* root) {
+        vector<vector<int> > ret;
+        buildVector(root, 0, ret);
+        return ret;
+    }
+    
