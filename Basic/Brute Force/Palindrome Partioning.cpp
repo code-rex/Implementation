791b9313a57d260c4997071a903f4bf875fde77a
Sahil kumar <sk19631999@gmail.com>
2019-09-20 02:28:14 +0530

    Create Palindrome Partioning.cpp

---
+++ Basic/Brute Force/Palindrome Partioning.cpp
@@ -0,0 +1,33 @@
+bool isPalindrome(string& s, int start, int end) {
+        while(start <= end) {
+            if(s[start++] != s[end--])
+                return false;
+        }
+        return true;
+    }
+    
+void palinpart(string& A,vector<string>&aux,vector<vector<string>>&ans,int start)
+{
+    if(start == A.length())
+    {
+        ans.push_back(aux);
+        return ;
+    }
+    for(int i=start;i<A.size();i++)
+    {
+        if(isPalindrome(A,start,i))
+        {
+            string k = A.substr( start, i-start+1 );
+            aux.push_back(k);
+            palinpart(A,aux,ans,i+1);
+            aux.pop_back();
+        }
+    }
+}
+
+vector<vector<string> > Solution::partition(string A) {
+    vector<vector<string>>ans;
+    vector<string>aux;
+    palinpart(A,aux,ans,0);
+    return ans;
+}
