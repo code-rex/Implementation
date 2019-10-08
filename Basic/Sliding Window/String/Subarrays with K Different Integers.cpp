65c034d61ae3df44701a33fdc85a2e3c3de602cb
Sahil kumar <sk19631999@gmail.com>
2019-10-08 14:09:36 +0530

    Create Subarrays with K Different Integers.cpp

---
+++ Basic/Sliding Window/String/Subarrays with K Different Integers.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/subarrays-with-k-different-integers/
+
+int subarraysWithKDistinct(vector<int>& A, int K) {
+        int begin=0,end=0,cnt=0,prefix=0,res=0;
+        vector<int> m(A.size()+1,0);  // change to unordered_map for bigger number
+        while(end<A.size()){
+            if(m[A[end++]]++==0) cnt++;
+            if(cnt>K){
+                --m[A[begin++]];
+                --cnt;
+                prefix=0;
+            }
+            while(m[A[begin]]>1) ++prefix,--m[A[begin++]];
+            if(cnt==K) res+=prefix+1;
+        }
+        return res;
+    }
