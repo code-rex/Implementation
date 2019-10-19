e1a5cc6cb1c28c136faa50adf58ada795238893e
Sahil kumar <sk19631999@gmail.com>
2019-10-20 02:17:00 +0530

    Update Word Break.cpp

--- Basic/Dynamic Programming/Word Break.cpp
+++ Basic/Dynamic Programming/Word Break.cpp
@@ -10,30 +10,21 @@ Return 1 ( corresponding to true ) because "myinterviewtrainer" can be segmented
 */
 
 
-bool search(string a,vector<string>&B)
-{
-    for(int i=0;i<B.size();i++)
-        if(a==B[i])
-            return true;
-    return false;
-}
-int Solution::wordBreak(string A, vector<string> &B) {
-    int n=A.size();
-    vector<int>dp(n+1,0);
-    dp[n]=1;
-    //traversing from back
-    for(int i=n-1;i>=0;i--)
-    {
-        for(int j=i;j<n;j++)
-        {
-            string p=A.substr(i,j-i+1);
-            
-            if(search(p,B) && dp[j+1]==1)
-            {
-                dp[i]=1;
-                break;
-            }
-        }
+bool wordBreak(string s, vector<string>& wordDict) {
+      int n=s.size();
+      unordered_set<string>set(wordDict.begin(),wordDict.end());
+      vector<int>dp(n+1,0);dp[n]=1;
+        
+      for(int i=n-1;i>=0;i--)
+          for(int j=i;j<n;j++)
+          {
+              string p=s.substr(i,j-i+1);
+              //This is valid string and beyond this is also valid
+              if(set.find(p)!=set.end() && dp[j+1]==1) 
+              {
+                  dp[i]=1;
+                  break;
+              }
+          }
+        return dp[0];
     }
-    return dp[0];
-}
