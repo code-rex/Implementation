1cf6b3b4cffa222ae3d2b5dd58cb14778a7ec0d0
Sahil kumar <sk19631999@gmail.com>
2019-12-04 03:10:41 +0530

    Update Sort By Colours.cpp

--- Basic/Sliding Window/Sort By Colours.cpp
+++ Basic/Sliding Window/Sort By Colours.cpp
@@ -1,9 +1,11 @@
-int low=0,mid=0,high=n-1;
-	while(mid<=high)
-	{
-		if(a[mid]==0)
-			swap(a[mid],a[low++]);
-		if(a[mid]==2)
-			swap(a[mid],a[high--]);
-		mid++;
-	}
+void sortColors(vector<int>& a) {
+        int low=0,mid=0,high=a.size()-1;
+	    while(mid<=high)
+        {
+            if(a[mid]==0)
+                swap(a[mid++],a[low++]);
+            else if(a[mid]==1)
+                mid++;
+            else if(a[mid]==2)
+                swap(a[mid],a[high--]);
+        }
