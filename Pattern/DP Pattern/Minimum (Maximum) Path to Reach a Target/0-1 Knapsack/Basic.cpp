bf5da63db2d38a0c02c9ec575b391d62d7c24b8d
Sahil kumar <sk19631999@gmail.com>
2019-12-28 02:13:12 +0530

    Create Basic.cpp

---
+++ Pattern/DP Pattern/Minimum (Maximum) Path to Reach a Target/0-1 Knapsack/Basic.cpp
@@ -0,0 +1,23 @@
+int n,W;
+int K[100][100];   
+// Returns the maximum value that can be put in a knapsack of capacity W 
+int knapSack(int wt[], int val[]) 
+{ 
+   int i, w; 
+  
+   // Build table K[][] in bottom up manner 
+   for (i = 0; i <= n; i++) 
+   { 
+       for (w = 0; w <= W; w++) 
+       { 
+           if (i==0 || w==0) 
+               K[i][w] = 0; 
+           else if (wt[i-1] <= w) 
+                 K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]); 
+           else
+                 K[i][w] = K[i-1][w]; 
+       } 
+   } 
+  
+   return K[n][W]; 
+} 
